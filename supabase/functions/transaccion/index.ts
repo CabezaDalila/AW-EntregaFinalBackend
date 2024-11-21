cd481870994431ea02122976a86c0316ea7bee29
dalila <dalilagiselcabeza@gmail.com>
2024-11-21 15:43:33 -0300

    transacciones

--- supabase/functions/transaccion/index.ts
+++ supabase/functions/transaccion/index.ts
@@ -67,41 +67,86 @@ const getDataPortfolioUser = async (req: Request): Promise<Response> => {
         status: 500,
       });
     }
+
     const portfolioId = Number(portfData.id);
     const { data: transaccionData, error: transaccionError } = await supabase
-    .from("transaccion")
-    .select("*")
-    .eq("id_portfolio", portfolioId)
-    .eq("tipoTransaccion", "buy");
-  
-  if (transaccionError) {
-    console.error("Error al calcular el total invertido:", transaccionError);
-    return new Response("Error al calcular total invertido", {
-      headers: corsHeaders,
-      status: 500,
-    });
-  }
-  
-  // Calculate total invested manually
-  const totalInvertido = transaccionData.reduce(
-    (sum, transaction) => sum + (transaction.cantidad * transaction.precio),
-    0
-  );
-  
-  // Devolvemos tanto el dinero disponible como el total invertido
-  return new Response(
-    JSON.stringify({
-      dineroDisponible: portfolioData.dineroDisponible,
-      totalInvertido,
-    }),
-    {
-      headers: { ...corsHeaders, "Content-Type": "application/json" },
-      status: 200,
+      .from("transaccion")
+      .select("*")
+      .eq("id_portfolio", portfolioId)
+      .eq("tipoTransaccion", "buy");
+
+    if (transaccionError) {
+      console.error(
+        "Error al calcular el total invertido de compra:",
+        transaccionError,
+      );
+      return new Response("Error al calcular total invertido de compra", {
+        headers: corsHeaders,
+        status: 500,
+      });
     }
-  );
+
+    const { data: transaccionSellData, error: transaccionSellError } =
+      await supabase
+        .from("transaccion")
+        .select("*")
+        .eq("id_portfolio", portfolioId)
+        .eq("tipoTransaccion", "sell");
+
+    if (transaccionSellError) {
+      console.error(
+        "Error al calcular el total invertido de ventas:",
+        transaccionSellError,
+      );
+      return new Response("Error al calcular total invertido de ventas", {
+        headers: corsHeaders,
+        status: 500,
+      });
+    }
+    const totalInvertidoBuy = transaccionData.reduce(
+      (sum, transaction) => sum + (transaction.cantidad * transaction.precio),
+      0,
+    );
+
+    const totalInvertidoSell = transaccionSellData.reduce(
+      (sum, transaction) => sum + (transaction.cantidad * transaction.precio),
+      0,
+    );
+    const totalInvertido = (totalInvertidoBuy - totalInvertidoSell) > 0
+      ? totalInvertidoBuy - totalInvertidoSell
+      : 0;
+
+    const { data: transaccionAllData, error: transaccionAllError } =
+      await supabase
+        .from("transaccion")
+        .select("*")
+        .eq("id_portfolio", portfolioId);
+
+    if (transaccionSellError) {
+      console.error(
+        "Error al calcular el total invertido:",
+        transaccionSellError,
+      );
+      return new Response("Error al calcular total invertido", {
+        headers: corsHeaders,
+        status: 500,
+      });
+    }
+    // Devolvemos tanto el dinero disponible como el total invertido
+    return new Response(
+      JSON.stringify({
+        dineroDisponible: portfolioData.dineroDisponible,
+        totalInvertido,
+        transaccionAllData,
+      }),
+      {
+        headers: { ...corsHeaders, "Content-Type": "application/json" },
+        status: 200,
+      },
+    );
   } catch (err) {
     console.error("Error en la función:", err);
-    return new Response("Error interno del servidor", {
+    return new Response("Error interno del servidor al obtener los datos", {
       headers: corsHeaders,
       status: 500,
     });
@@ -109,7 +154,6 @@ const getDataPortfolioUser = async (req: Request): Promise<Response> => {
 };
 
 const putBuy = async (req: Request): Promise<Response> => {
-  console.log("SI entro");
   try {
     console.log("Request recibido:", req);
     const body = await req.json();
@@ -155,8 +199,122 @@ const putBuy = async (req: Request): Promise<Response> => {
         status: 404,
       });
     }
+    if ((portfolioData.dineroDisponible - (precio * cantidad)) >= 0) {
+      const nuevoDineroDisponible = portfolioData.dineroDisponible -
+        (precio * cantidad);
+      //modificas el dinero disponible
+      await supabase
+        .from("portfolio")
+        .update({ dineroDisponible: nuevoDineroDisponible })
+        .eq("id_user", userId);
 
-    const nuevoDineroDisponible = portfolioData.dineroDisponible -
+      // idPorfolio
+      const { data: portfData, error: portfError } = await supabase
+        .from("portfolio")
+        .select("id")
+        .eq("id_user", userId)
+        .single();
+
+      if (portfError || !portfData) {
+        console.error("Error al obtener el id del portfolio:", portfError);
+        return new Response("Error al obtener portfolio", {
+          headers: corsHeaders,
+          status: 500,
+        });
+      }
+      const portfolioId = Number(portfData.id);
+
+      const transaccionBuy = {
+        tipoTransaccion: "buy",
+        ticker: ticker,
+        cantidad: cantidad,
+        precio: precio,
+        id_portfolio: portfolioId,
+      };
+
+      const { data: transactionData, error: transactionError } = await supabase
+        .from("transaccion")
+        .insert(transaccionBuy)
+        .select();
+
+      if (transactionError || !transactionData) {
+        console.error("Error al insertar transacción:", transactionError);
+        return new Response("Error al insertar transacción", {
+          headers: corsHeaders,
+          status: 500,
+        });
+      }
+    }else{
+      return new Response("Error al insertar transacción, dinero insuficiente", {
+        headers: corsHeaders,
+        status: 500,
+      });
+    }
+
+    return new Response(
+      JSON.stringify({ mensaje: "Compra actualizada con éxito" }),
+      {
+        headers: { ...corsHeaders, "Content-Type": "application/json" },
+        status: 200,
+      },
+    );
+  } catch (err) {
+    console.error("Error en la función:", err);
+    return new Response("Error interno del servidor al realizar la compra", {
+      headers: corsHeaders,
+      status: 500,
+    });
+  }
+};
+
+const patchSell = async (req: Request): Promise<Response> => {
+  try {
+    console.log("Request recibido:", req);
+    const body = await req.json();
+    const { tipoTransaccion, ticker, cantidad, precio, email } = body;
+
+    if (!ticker || !precio || !cantidad) {
+      return new Response("Faltan datos de la venta", {
+        headers: corsHeaders,
+        status: 400,
+      });
+    }
+    if (!email) {
+      return new Response("Falta el email del usuario", {
+        headers: corsHeaders,
+        status: 400,
+      });
+    }
+    const { data: userData, error: userError } = await supabase
+      .from("users")
+      .select("id")
+      .eq("email", email)
+      .single();
+
+    if (userError || !userData) {
+      console.error("Error al obtener el usuario:", userError);
+      return new Response("Usuario no encontrado", {
+        headers: corsHeaders,
+        status: 404,
+      });
+    }
+    const userId = userData.id;
+
+    const { data: portfolioData, error: portfolioError } = await supabase
+      .from("portfolio")
+      .select("dineroDisponible")
+      .eq("id_user", userId)
+      .single();
+
+    if (portfolioError || !portfolioData) {
+      console.error("Error al obtener el portfolio:", portfolioError);
+      return new Response("Portfolio no encontrado", {
+        headers: corsHeaders,
+        status: 404,
+      });
+    }
+
+    const nuevoDineroDisponible = portfolioData.dineroDisponible +
       (precio * cantidad);
     //modificas el dinero disponible
     await supabase
@@ -180,8 +338,8 @@ const putBuy = async (req: Request): Promise<Response> => {
     }
     const portfolioId = Number(portfData.id);
 
-    const transaccionBuy = {
-      tipoTransaccion: "buy",
+    const transaccionSell = {
+      tipoTransaccion: "sell",
       ticker: ticker,
       cantidad: cantidad,
       precio: precio,
@@ -190,7 +348,7 @@ const putBuy = async (req: Request): Promise<Response> => {
 
     const { data: transactionData, error: transactionError } = await supabase
       .from("transaccion")
-      .insert(transaccionBuy)
+      .insert(transaccionSell)
       .select();
 
     if (transactionError || !transactionData) {
@@ -201,7 +359,7 @@ const putBuy = async (req: Request): Promise<Response> => {
       });
     }
     return new Response(
-      JSON.stringify({ mensaje: "Compra actualizada con éxito" }),
+      JSON.stringify({ mensaje: "Venta actualizada con éxito" }),
       {
         headers: { ...corsHeaders, "Content-Type": "application/json" },
         status: 200,
@@ -209,7 +367,7 @@ const putBuy = async (req: Request): Promise<Response> => {
     );
   } catch (err) {
     console.error("Error en la función:", err);
-    return new Response("Error interno del servidor", {
+    return new Response("Error interno del servidor al realizar la venta", {
       headers: corsHeaders,
       status: 500,
     });
@@ -221,13 +379,14 @@ Deno.serve((req: Request) => {
     // Manejo de preflight para solicitudes CORS
     return new Response("ok", { headers: corsHeaders });
   }
-
   const { method } = req;
   switch (method) {
     case "GET":
       return getDataPortfolioUser(req);
     case "PUT":
       return putBuy(req);
+    case "PATCH":
+      return patchSell(req);
     default:
       return new Response("Método no permitido", {
         headers: corsHeaders,
